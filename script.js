90c749246971c7ab0a7f096f2310c364a2ddc312
Ali Andrea <99283192+AliAndrea1@users.noreply.github.com>
2024-10-04 17:36:56 -0300

    Atualizando arquivos

---
+++ script.js
@@ -0,0 +1,4 @@
+// Adiciona um listener de evento ao texto clicável
+document.getElementById('addToPlaylist').addEventListener('click', function() {
+    alert('Adicionado à playlist!'); // Mensagem que será exibida
+});
